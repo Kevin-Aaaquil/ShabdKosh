01dd074f7ddc1516ca119df17a190edf1d82a7e8
Kritika Malhotra <73088880+kritical1326@users.noreply.github.com>
2021-07-03 15:07:10 +0530

    Create index.py
    Make way, I'm here!

---
+++ index.py
@@ -0,0 +1,25 @@
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+  print('{0.user} is active now.'.format(client))
+  #await client.change_presence(activity = discord.Game(''))
+
+@client.event
+async def on_guild_join(guild):
+  channel = await guild.create_text_channel('shabdkosh')
+  await channel.send("Hello! I'm ShabdKosh, your personal word assistant!")
+
+channels = ['shabdkosh']
+
+@client.event
+async def on_message(message):
+  if str(message.channel) in channels:
+    if message.author == client.user:
+      return
+
+    if message.content.startswith('Hello'):
+      await message.channel.send('Hi!')
+      
