245979a682bfc3eee084599abb2efd2588a9a9ac
Kritika Malhotra <73088880+kritical1326@users.noreply.github.com>
2021-07-04 02:03:56 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,7 +1,20 @@
-# import discord dependencies from pip
+import os
+import sox
 import discord
+import requests
+import json
+import asyncio
+from discord.ext import commands
+import googletrans
+from googletrans import Translator
+from keep_alive import keep_alive
+from google_speech import Speech # for voice module
+import languages_supported.py as ls
+
 
 client = discord.Client()
+
+translator = googletrans.Translator()
 
 @client.event
 async def on_ready():
@@ -21,22 +34,25 @@ async def on_message(message):
     if message.author == client.user:
       return
 
-    if message.content.startswith('Hello'):
-      await message.channel.send('Hi')
-      
-    if message.content.startswith('!tle'):
-      msg = message.content.split("\"")
-      print(msg)
-      tle = translator.translate(msg[1], dest=msg[2].lstrip())
-      await message.channel.send("`{}` -> `{}`".format(msg[1], tle.text))
+    if message.content.startswith('*Hello'):
+      await message.channel.send('https://tenor.com/view/hello-there-gif-9442662')
+      print(googletrans.LANGUAGES)
+      #url = 'https://g.tenor.com/v1/search?q=hello&key={os.environ['TENOR']}&limit=8'
+      #response = await requests.get(url)
+      #json_data = json.loads(response.text)
+      #print(json_data)
     
-    if message.content.startswith('!dt'):
+    if message.content.startswith('*tle'):
+          msg = message.content.split("\"")
+          print(msg)
+          tle = translator.translate(msg[1], dest=msg[2].lstrip())
+          await message.channel.send("`{}` -> `{}`".format(msg[1], tle.text))
+
+    if message.content.startswith('*dt'):
       msg = message.content[8:]
       await message.channel.send("The message is in {}".format({translator.detect(msg)}))
-      
+        
 keep_alive()
 
 my_secret = os.environ['TOKEN']
 client.run(my_secret)
-      
-      
