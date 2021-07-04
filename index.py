02be8da2a12ba2af5c0ac91d0734326f82f4b50b
Tanmay Sharma <tanmaysharmav@gmail.com>
2021-07-04 09:06:05 +0530

    Update index.py

--- index.py
+++ index.py
@@ -6,6 +6,7 @@ import json
 import asyncio
 from discord.ext import commands
 import googletrans
+from datetime import datetime
 from googletrans import Translator
 from keep_alive import keep_alive
 from google_speech import Speech # for voice module
@@ -24,9 +25,27 @@ async def on_ready():
 @client.event
 async def on_guild_join(guild):
   channel = await guild.create_text_channel('shabdkosh')
-  await channel.send("Hello! I'm ShabdKosh, your personal word assistant!")
+  guild = client.get_guild(#guildid)
+  channel = guild.get_channel(#channelid)
+  embed=discord.Embed(
+        title="Hello! Hola! Namaste!",
+        description="I am Shabdkosh! Your personal voice and text translator bot :nerd: ",
+        color=0x0000FF,
+        timestamp=datetime.utcnow()
+    )
+    embed.add_field(name="Name", 
+                    value=f'ShabdKosh', 
+                    inline=True)
+    embed.add_field(name="Who am I ?", 
+                    value=f'lalalala ',
+                    inline=False)
+    embed.set_footer(text="                  ")
+    embed.set_image(url="https://media.giphy.com/media/IgGcxqawkRc6y43Z6I/giphy.gif")
+    await channel.send(embed=embed)
 
+    
 channels = ['shabdkosh']
+
 
 @client.event
 async def on_message(message):
@@ -65,7 +84,9 @@ async def on_message(message):
     #   lcode = s.join(lang)
       print(lcode)
       await message.channel.send("The message is in {}".format(ldict.get(lcode)))
-        
+
+      
+      
 keep_alive()
 
 my_secret = os.environ['TOKEN']
