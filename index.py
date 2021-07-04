f135906d4a2e9141ffa9d3315d8f340ff080561e
Kritika Malhotra <73088880+kritical1326@users.noreply.github.com>
2021-07-04 16:10:43 +0530

    Update index.py

--- index.py
+++ index.py
@@ -2,13 +2,13 @@ import os
 import discord
 import googletrans
 from datetime import datetime
-from google_speech import Speech  # for voice module
+#from google_speech import Speech  # for voice module
 from gif import giffy
-from discord.flags import Intents
-import speech_recognition as spr
-from googletrans import Translator
-from discord.ext import commands
-from gtts import gTTS
+#from discord.flags import Intents
+#import speech_recognition as spr
+#from googletrans import Translator
+#from discord.ext import commands
+#from gtts import gTTS
 
 
 
@@ -40,9 +40,9 @@ async def on_message(message):
         if message.author == client.user:
             return
 
-        if message.content.startswith('*Hello'):
+        if message.content.startswith('*hello'):
             await message.channel.send(await giffy())
-            await message.channel.send(googletrans.LANGUAGES)
+            #await message.channel.send(googletrans.LANGUAGES)
 
         if message.content.startswith('*tle'):
             msg = message.content.split("\"")
@@ -77,84 +77,16 @@ async def on_message(message):
             msg = message.content[5:]
             await message.channel.send(dictionary.antonym(msg))
 
-#         if message.content.startswith('*help'):
-#           async def help(ctx):
-#             author = ctx.message.author
-            
-#             embed = discord.Embed(colour = discord.Colour.yellow())
+        if message.content.startswith('*lang'):
+          dict = googletrans.LANGUAGES
+          await message.channel.send("The languages available in ShabdKosh are:\n")
+          ln = ""
+          for i in dict.items():
+            ln = ln + str(i) + "\n"
+          await message.channel.send(ln)      
 
-#             embed.set_author(name='Help')
-#             embed.add_field(name='*hello',value = 'Returns a hello!',inline = False)
-          
-      
-@client.command(pass_context = True)
-async def join(ctx):
-    if (ctx.author.voice):
-        channel = ctx.message.author.voice.channel
-        await channel.connect()
-    else:
-        await ctx.send("You are not in a voice channel, you must be in a voice channel to run this command")
-
-
-@client.command(pass_context = True)
-async def leave(ctx):
-    if (ctx.voice_client):
-        await ctx.guild.voice_client.disconnect()
-        await ctx.send("I left the voice channel")
-    else:
-        await ctx.send("I am not in a voice channel")
-
-
-intents = discord.Intents.default()
-
-client = commands.Bot(command_prefix = '*', intents=intents)
-
-
-
-@client.event
-async def on_message(message):
-    if str(message.channel) in channel:
-        if message.author == client.user:
-            return
-        
-        recog1 = spr.Recognizer()
-        recog2 = spr.Recognizer()
-
-        mc = spr.Microphone(device_index=0)
-        
-        if message.content.startswith('*voice'):
-          
-          with mc as source:
-            print("Speak 'Hello' to initiate the Translation!")
-            print("----------------------------")
-            audio = recog1.listen(source)
-          if 'hello' in recog1.recognize_google(audio):
-            recog1 = spr.Recognizer()
-            translator = Translator()
-            from_lang = 'en'
-            to_lang = 'hi'
-            with mc as source:
-              print('Speak a sentence...')
-              audio = recog2.listen(source)
-              get_sentence = recog2.recognize_google(audio)
-        
-            try:
-              get_sentence = recog2.recognize_google(audio)
-              print('Phrase to be Tranlated: '+ get_sentence)
-              text_to_translate = translator.translate(get_sentence,dest = to_lang, src = from_lang) 
-              #print(text_to_translate.text)
-              text = text_to_translate.text
-              speak = gTTS(text=text, lang = to_lang, slow = False)
-              speak.save("captured_voice.mp3")
-              os.system("start captured_voice.mp3")
-            except spr.UnknownValueError:
-              print("Unable to understand the input")
-            except spr.RequestError as e:
-              print("Unable to provide required output".format(e))
-          
-
-
-keep_alive()
+        if message.content.startswith('*help'):
+          await message.channel.send(" ```*help - Displays the commands of ShabdKosh📚 \n*hello- Pops in to say Hi!📘 \n*lang - Displays the languages in ShabdKosh's pocket!📖 \n*tle \"sentence\" lang_code- Translates words from 100+ languages📗 \n*dt sentence - Detects the language of the sentence📙 \n*dict word - Displays the meaning of the word in English✨ \n*syn word - Gives synonyms of the word in English🔖 \n*ant word - Gives antonyms of the word in English📑 ``` ")
 
 my_secret = os.environ['TOKEN']
 client.run(my_secret)
