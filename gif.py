3fb564f57aad97f501cdcf03857267e07d19775f
Kritika Malhotra <73088880+kritical1326@users.noreply.github.com>
2021-07-04 15:37:04 +0530

    Create gif.py

---
+++ gif.py
@@ -0,0 +1,16 @@
+import random
+
+gif = ["https://tenor.com/view/bad-teeth-hi-hello-wave-gif-14630063",
+"https://tenor.com/view/cat-cute-animals-hello-there-gif-11875188",
+"https://tenor.com/view/mandalorian-baby-yoda-hello-gif-19013340",
+"https://tenor.com/view/party-audrey-nethery-hair-flip-fabulous-kareoke-gif-10860579",
+"https://tenor.com/view/puppy-dog-cute-head-tilt-hi-gif-13525498",
+"https://tenor.com/view/hello-hi-joker-gif-3565048",
+"https://tenor.com/view/hi-jim-carrey-beach-wtf-gif-5615667",
+"https://tenor.com/view/you-alive-hello-finding-nemo-poke-braces-gif-5257087",
+"https://tenor.com/view/hello-there-gif-9442662",
+"https://tenor.com/view/well-hello-there-pay-attention-gif-9340416"]
+
+async def giffy():
+  return(random.choice(gif))
+  
