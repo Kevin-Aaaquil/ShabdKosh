d416fb29638ce619c583351f42dfd98066d231d5
Tanmay Sharma <tanmaysharmav@gmail.com>
2021-07-04 13:19:08 +0530

    Update speech.py

--- speech.py
+++ speech.py
@@ -1,7 +1,16 @@
+from discord.flags import Intents
 import speech_recognition as spr
+import discord
 from googletrans import Translator
+from discord.ext import commands
 from gtts import gTTS
 import os
+
+intents = discord.Intents.default()
+
+client = commands.Bot(command_prefix = '*', intents=intents)
+
+
 
 recog1 = spr.Recognizer()
 recog2 = spr.Recognizer()
@@ -28,8 +37,8 @@ if 'hello' in recog1.recognize_google(audio):
         try:
             get_sentence = recog2.recognize_google(audio)
             print('Phrase to be Tranlated: '+ get_sentence)
-            text_to_translate = translator.translate(get_sentence,dest = to_lang, src = from_lang) #, dest = to_lang)
-            print(text_to_translate.text)
+            text_to_translate = translator.translate(get_sentence,dest = to_lang, src = from_lang) 
+            #print(text_to_translate.text)
             text = text_to_translate.text
             speak = gTTS(text=text, lang = to_lang, slow = False)
             speak.save("captured_voice.mp3")
@@ -38,3 +47,22 @@ if 'hello' in recog1.recognize_google(audio):
             print("Unable to understand the input")
         except spr.RequestError as e:
             print("Unable to provide required output".format(e))
+
+
+
+@client.command(pass_context = True)
+async def join(ctx):
+    if (ctx.author.voice):
+        channel = ctx.message.author.voice.channel
+        await channel.connect()
+    else:
+        await ctx.send("You are not in a voice channel, you must be in a voice channel to run this command")
+
+
+@client.command(pass_context = True)
+async def leave(ctx):
+    if (ctx.voice_client):
+        await ctx.guild.voice_client.disconnect()
+        await ctx.send("I left the voice channel")
+    else:
+        await ctx.send("I am not in a voice channel")
